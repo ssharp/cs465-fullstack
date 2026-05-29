4950b1723b09e72f0033d41491edc23a4441b993
Seth Sharp <seth.sharp1@snhu.edu>
2026-05-29 16:55:38 -0500

    Module 2 corrected hyperlink usage for URL

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // Register handlebars partials (https://www.nmjs.com/package/habs)
-handlebars.registerPartials(__dirname, + '/app_server/views/partials');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
